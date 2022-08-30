0caed9d90459eaf03ec91924b4e9bdff79ba01e6
Hayley Ashby <hayleyashby7@gmail.com>
2022-08-30 15:57:35 +0100

    Moved opening hours from contact to about. Restyled contact section. About section still WIP

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,4 +1,3 @@
-import openData from "../data/hours.json";
 import contactData from "../data/contact.json";
 import emailSVG from "../images/email.svg";
 import phoneSVG from "../images/phone.svg";
@@ -8,9 +7,7 @@ export function contactContent() {
 	const contactDiv = document.createElement("div");
 
 	contactDiv.classList.add("content-text", "contact-content");
-
 	contactDiv.appendChild(addMap());
-	contactDiv.appendChild(addHours());
 	contactDiv.appendChild(addContactDetails());
 
 	return contactDiv;
@@ -32,39 +29,6 @@ function addMap() {
 	map.appendChild(mapImage);
 
 	return map;
-}
-
-function addHours() {
-	const hours = document.createElement("div");
-	hours.classList.add("hours");
-
-	const header = document.createElement("h1");
-	header.innerHTML = "Opening Times";
-	hours.appendChild(header);
-
-	const openingTimes = document.createElement("table");
-	hours.appendChild(openingTimes);
-	parseHours(openingTimes);
-
-	return hours;
-}
-
-function parseHours(table) {
-	const dailyTimes = openData.OpeningHours;
-
-	for (const daily of dailyTimes) {
-		let dayRow = document.createElement("tr");
-
-		let day = document.createElement("td");
-		day.innerHTML = daily.Day;
-		dayRow.appendChild(day);
-
-		let times = document.createElement("td");
-		times.innerHTML = daily.Hours;
-		dayRow.appendChild(times);
-
-		table.appendChild(dayRow);
-	}
 }
 
 function addContactDetails() {
